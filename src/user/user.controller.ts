cf82224744405df7a9db6d9a54fc276bf6cc5b99
ahmedalaa2544 <ahmedalaa2544@yahoo.com>
2024-08-06 18:30:24 +0300

    message and user schema

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { AuthenticationGuard } from '../guards/authentication/authentication.guard';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post('signUp')
+  signup(@Body() body: any) {
+    return this.userService.signUp(body);
+  }
+}
